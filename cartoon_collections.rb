3a594e2a6cecece77f87b5fef6549f73d449ca21
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 15:32:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,6 @@ end
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
   contains_cheddar = ["banana", "cheddar", "sock"]
-  contains_cheddar
+  contains_cheddar.find
     
 end

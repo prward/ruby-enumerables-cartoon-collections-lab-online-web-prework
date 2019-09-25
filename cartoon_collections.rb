50142e8c0e6dd38d66a6613767e3577db16fe151
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 14:47:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves (dwarves)
 end
 
 def summon_captain_planet (veggies)
- veggies = ["carrot!", "cucumber!", "pepper!"]
+ veggies = ["apple!", "carrot!", "cucumber!", "pepper!"]
  veggies.map(&:capitalize)
 end
 

4a598c23d636b8b14abc555284d6fb9d00b20939
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 14:35:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves (dwarves)
     
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (veggies)
  veggies = ["carrot", "cucumber", "pepper"]
 end
 

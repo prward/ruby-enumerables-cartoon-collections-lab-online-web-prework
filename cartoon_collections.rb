3268d2333451dd94a809dc94fe036a96b9fd5864
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 14:23:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves (dwarves)
   dwarves = ["Dopey", "Bashful", "Grumpy"]
   dwarves.each_with_index do |x, i|
-    puts {i} => {x}
+    puts {i: x}
 end
 
 def summon_captain_planet# code an argument here

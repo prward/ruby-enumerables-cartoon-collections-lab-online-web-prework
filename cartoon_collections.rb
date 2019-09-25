d32ecb8ade91da3e035941b4fae442d9410329ef
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 15:15:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(calls_long)
  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_types)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end

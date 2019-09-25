bbc377bce7d89f9ca4a7b1afede45c52245ac1e5
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-25 13:46:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves (dwarves)
-   dwarves = ["Doc", "Bashful", "Grumpy"]
+   dwarves = ["Dopey", "Doc", "Bashful", "Grumpy"]
   dwarves.each_with_index { |items, index| p "#index :#items" }
   
 end
